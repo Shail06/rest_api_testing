2582d760e5fb7ce1f39352fdd6bacdca964c91f9
shail06 <shailesh.wadhwa@gmail.com>
2025-02-01 01:10:02 +0000

    wip: token handling

--- api/orders_api.py
+++ api/orders_api.py
@@ -1,6 +1,14 @@
 from api.base_api import BaseApi
+import json
 
 class OrdersApi(BaseApi):
     
+    def __init__(self, auth_token=None):
+        super().__init__()
+        self.auth_token = auth_token
+    
     def create_order(self, order_data):
-        return self.post('/api/v1/orders/create', order_data)
+        headers = {"Content-Type": "application/json"}
+        if self.auth_token:
+            headers["Authorization"] = f"Bearer {self.auth_token}"
+        return self.post('/api/v1/orders/create', request_body=order_data, headers=headers)
